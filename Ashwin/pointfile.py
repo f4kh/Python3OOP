7c31fe3a1149884c27128bae10819ee8e1d13662
f4kh <f4khri@gmx.com>
2019-02-16 19:12:51 +0100

    lesson3 methods

--- Ashwin/pointfile.py
+++ Ashwin/pointfile.py
@@ -5,7 +5,14 @@ Created on Feb 16, 2019
 '''
 
 class Point:
-    pass
+    
+    def assign(self,x,y,z):
+        self.x = x
+        self.y = y 
+        self.z = z 
+        
+    def printPoint(self):
+        print(self.x, self.y, self.z)
 
 
 
@@ -14,16 +21,21 @@ p1 = Point()
 p2 = Point()
 
 #we do not need to declare attributes in class in advance like other oop languagges java 4exp . :)
-p1.x = 2
-p1.y = 4
-p1.z = 6
+# p1.x = 2
+# p1.y = 4
+# p1.z = 6
+# 
+# p2.x = 1
+# p2.y = 3
+# p2.z = 5
 
-p2.x = 1
-p2.y = 3
-p2.z = 5
+p1.assign(4, 5, 6)
+p1.printPoint()
+p2.assign(2, 8, 1)
+p2.printPoint()
 
 #address of object in memory
 print(p1)
-print(p1.x,p1.y,p1.z)
+# print(p1.x,p1.y,p1.z)
 print(p2) 
-print(p2.x,p2.y,p2.z)
+# print(p2.x,p2.y,p2.z)

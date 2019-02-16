205249c103cbd3ad0aa14de6d06568809268eb8d
f4kh <f4khri@gmx.com>
2019-02-16 15:38:58 +0100

    lesson1

---
+++ Ashwin/pointfile.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 16, 2019
+
+@author: fakhri
+'''
+
+class Point:
+    pass
+
+
+
+#2 objects
+p1 = Point()
+p2 = Point()
+
+
+#address of object in memory
+print(p1)
+print(p2) 
